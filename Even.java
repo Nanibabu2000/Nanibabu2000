54cbd7f976156a2219f53749f6c3a17762d35fda
Nanibabu2000 <panchadarlananibabu@gmail.com>
2024-07-11 17:30:30 +0530

    first commit

---
+++ Even.java
@@ -0,0 +1,5 @@
+public class Even{
+p s v m(String args[]){
+int n=80;
+if(n%2==0)
+System.out.println("given number is even");
---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Mulitiply
+{
+public static void main(String args[]){
+int n=3;
+int v=7;
+int res=n*v;
+System.out.println("multiply="res);
